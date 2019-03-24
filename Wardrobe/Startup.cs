c9ed9e41f9a69f7ad649ca841922d833dda53220
Alyzande Renard <alyzande@gmail.com>
2019-03-24 16:39:30 +0000

    Add project files.

---
+++ Wardrobe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wardrobe.Startup))]
+namespace Wardrobe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
