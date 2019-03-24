6a682d505872b81afd235c3796343ab7ede54202
Alyzande Renard <alyzande@gmail.com>
2019-03-24 17:59:03 +0000

    igarment

---
+++ Wardrobe.Services/IService/IGarmentService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wardrobe.Services.IService
+{
+    public interface IGarmentService
+    {
+        IList<Wardrobe.Data.Garment> GetGarments();
+    }
+}
---
+++ Wardrobe.Services/Service/GarmentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wardrobe.Data;
+using Wardrobe.Data.IDAO;
+using Wardrobe.Data.DAO;
+
+namespace Wardrobe.Services.Service
+{
+    public class GarmentService : Wardrobe.Services.IService.IGarmentService
+
+    {
+        private IGarmentDAO _garmentDAO; 
+        public GarmentService()
+        {
+            _garmentDAO = new GarmentDAO();
+        }
+    }
+}
